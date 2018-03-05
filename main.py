1aae1ee22f804c16c2930beeeba1b18a8e355f2b
Conch <Conchsk@outlook.com>
2018-03-05 11:33:17 +0800

    20180305

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 
 from gorule import GoRule
+from resnet import ResNet
+
 from flask import Flask, request, send_file
 from flask_socketio import SocketIO, emit
 
@@ -13,6 +15,7 @@ io = SocketIO(app)
 
 board_size = 9
 go_rule = GoRule(board_size)
+res_net = ResNet(board_size)
 
 black_boards = [np.zeros((board_size, board_size), dtype=int) for _ in range(8)]
 white_boards = [np.zeros((board_size, board_size), dtype=int) for _ in range(8)]
@@ -20,13 +23,8 @@ turn = 'black'
 
 
 def _current_board():
-    tmp_board = np.zeros((board_size, board_size), dtype=int)
-    for row in range(board_size):
-        for col in range(board_size):
-            if black_boards[0][row, col] == 1:
-                tmp_board[row, col] = 1
-            elif white_boards[0][row, col] == 1:
-                tmp_board[row, col] = -1
+    tmp_board = black_boards[0].copy()
+    tmp_board -= white_boards[0]
     return tmp_board
 
 
@@ -98,12 +96,14 @@ def test():
 
 
 @io.on('move2')
-def move2(row, col):
+def move2(message):
     global turn
 
-    row = int(request.values.get('row'))
-    col = int(request.values.get('col'))
+    row = message['row']
+    col = message['col']
+    print(f'{row}, {col}')
 
+    # player go
     result = go_rule.move(_board_rearrange(), row * board_size + col)
     if result is not None:
         Xtp1_board, Ytp1_board = result
@@ -117,8 +117,34 @@ def move2(row, col):
             black_boards.insert(0, Ytp1_board)
             white_boards.insert(0, Xtp1_board)
             turn = 'black'
+        emit('move2', json.dumps(_current_board().tolist()))
+    else:
+        return
+
+    # ai go
+    policy, value = res_net.predict([_board_rearrange()])
+    policy_dist = policy[0, :]
+    while True:
+        index = np.where(policy_dist == policy_dist.max())[0]
+        result = go_rule.move(_board_rearrange(), index)
+        if result is not None:
+            Xtp1_board, Ytp1_board = result
+            black_boards.pop()
+            white_boards.pop()
+            if turn == 'black':
+                black_boards.insert(0, Xtp1_board)
+                white_boards.insert(0, Ytp1_board)
+                turn = 'white'
+            else:
+                black_boards.insert(0, Ytp1_board)
+                white_boards.insert(0, Xtp1_board)
+                turn = 'black'
+            break
+        else:
+            policy_dist[index] = 0.0
+    emit('move2', json.dumps(_current_board().tolist()))
 
 
 if __name__ == '__main__':
-    # io.run(app)
-    app.run('127.0.0.1', port=5000)
+    io.run(app, host='127.0.0.1', port=5000)
+    # app.run('127.0.0.1', port=5000)
--- resnet.py
+++ resnet.py
@@ -1,31 +1,71 @@
+# -*- coding: UTF-8 -*-
+import random
+
 import tensorflow as tf
 
 
 class ResNet:
-    def __init__(self):
-        None
+    def __init__(self, board_size):
+        self.board_size = board_size
+
+        self.boards = tf.placeholder(tf.float32, shape=[None, self.board_size, self.board_size, 17])
+        self.policy = tf.placeholder(tf.float32, shape=[None, self.board_size * self.board_size])
+        self.value = tf.placeholder(tf.float32, shape=[None, 1])
+
+        first_layer = tf.layers.conv2d(self.boards, 256, 3, padding='same')
+        first_layer = tf.layers.batch_normalization(first_layer)
+        first_layer = tf.nn.relu(first_layer)
+
+        res_net_tower = first_layer
+        for i in range(10):
+            res_net_tower = self._res_block(res_net_tower)
+
+        self.policy_predict = self._policy_head(res_net_tower)
+        self.value_predict = self._value_head(res_net_tower)
+
+        self.loss = tf.reduce_mean(
+            tf.square(self.value - self.value_predict) - self.policy * tf.log(self.policy_predict))
+        self.train_step = tf.train.AdamOptimizer().minimize(self.loss)
+
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
 
     def _res_block(self, input_tensor):
-        l1_a = tf.layers.conv2d(inputs=input_tensor, filters=256, kernel_size=3)
-        l1_b = tf.layers.batch_normalization(inputs=l1_a)
-        l1_c = tf.nn.relu(features=l1_b)
-        l2_a = tf.layers.conv2d(inputs=l1_c, filters=256, kernel_size=3)
-        l2_b = tf.layers.batch_normalization(inputs=l2_a)
-        l2_c = tf.nn.relu(features=tf.add(input_tensor, l2_b))
-        return l2_c
+        conv1 = tf.layers.conv2d(input_tensor, 256, 3, padding='same')
+        conv1 = tf.layers.batch_normalization(conv1)
+        conv1 = tf.nn.relu(conv1)
+        conv2 = tf.layers.conv2d(conv1, 256, 3, padding='same')
+        conv2 = tf.layers.batch_normalization(conv2)
+        conv2 = tf.nn.relu(tf.add(input_tensor, conv2))
+        return conv2
 
     def _policy_head(self, input_tensor):
-        l1_a = tf.layers.conv2d(inputs=input_tensor, filters=2, kernel_size=1)
-        l1_b = tf.layers.batch_normalization(inputs=l1_a)
-        l1_c = tf.nn.relu(features=l1_b)
-        l2_a = tf.nn.softmax()
+        conv = tf.layers.conv2d(input_tensor, 2, 1, padding='same')
+        conv = tf.layers.batch_normalization(conv)
+        conv = tf.nn.relu(conv)
+        conv_flat = tf.reshape(conv, shape=[-1, self.board_size * self.board_size * 2])
+        policy_predict = tf.layers.dense(conv_flat, self.board_size * self.board_size + 1)
+        return policy_predict
 
     def _value_head(self, input_tensor):
-        l1_a = tf.layers.conv2d(inputs=input_tensor, filters=1, kernel_size=1)
-        l1_b = tf.layers.batch_normalization(inputs=l1_a)
-        l1_c = tf.nn.relu(features=l1_b)
+        conv = tf.layers.conv2d(input_tensor, 1, 1, padding='same')
+        conv = tf.layers.batch_normalization(conv)
+        conv = tf.nn.relu(conv)
+        conv_flat = tf.reshape(conv, shape=[-1, self.board_size * self.board_size])
+        fc1 = tf.layers.dense(conv_flat, 256)
+        fc1 = tf.nn.relu(fc1)
+        fc2 = tf.layers.dense(fc1, 1)
+        value_predict = tf.nn.tanh(fc2)
+        return value_predict
 
+    def train(self, boards_batch, policy_batch, value_batch):
+        self.sess.run(self.train_step, feed_dict={
+            self.boards: boards_batch,
+            self.policy: policy_batch,
+            self.value: value_batch
+        })
 
     def predict(self, boards):
-
-
+        policy_predict, value_predict = self.sess.run([self.policy_predict, self.value_predict],
+                                                      feed_dict={self.boards: boards})
+        return policy_predict, value_predict
